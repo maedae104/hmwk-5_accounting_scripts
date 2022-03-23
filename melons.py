2fb11f8ad6e3169925200ac4e949840fb6241f42
Mae Dae <maedae@136-25-136-235.cab.webpass.net>
2022-03-22 19:11:28 -0700

    completed function

--- melons.py
+++ melons.py
@@ -28,3 +28,41 @@ rind_color = {}
 
 average_weight = {}
 
+melons = {
+    "Honeydew" : {
+        "price" : 0.99,
+        "seedless" : True,
+        "flesh_color" : None,
+        "rind_color" : None,
+        "average_weight" : None} ,
+
+    # "Crenshaw" : {
+    #     "price" : 2.00
+    #     "seedless" : False
+    #     'flesh_color': None,
+    #     'rind_color': None,
+    #     'average_weight': None,} ,
+
+    'Crane': {
+        'price': 2.50,
+        'seedless': False,
+        'flesh_color': None,
+        'rind_color': None,
+        'average_weight': None,
+    },
+
+    'Casaba': {
+        'price': 2.50,
+        'seedless': False,
+        'flesh_color': None,
+        'rind_color': None,
+        'average_weight': None,
+    },
+
+    'Cantaloupe': {
+        'price': 0.99,
+        'seedless': False,
+        'flesh_color': None,
+        'rind_color': None,
+        'average_weight': None,
+    }}
